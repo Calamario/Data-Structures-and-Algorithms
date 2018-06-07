7213022f40db3fce54a65137a1ec1db68239fdf6
Calamario <mario.nishio@gmail.com>
2018-06-07 10:10:12 -0700

    redid gitignore, working on whiteboard 3

--- Challenges/Challenge_Adjacent_Product/Challenge_Adjacent_Product/Program.cs
+++ Challenges/Challenge_Adjacent_Product/Challenge_Adjacent_Product/Program.cs
@@ -10,8 +10,38 @@ namespace Challenge_Adjacent_Product
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            Console.ReadKey();
+            int[,] array2D = new int[3, 3] { { 1, 2, 5 }, { 3, 4, 8 }, { 5, 6, 7 } };
+            int rtnInt = AdajcentProduct(array2D);
+            Console.WriteLine(rtnInt);
+        }
+
+        static int AdajcentProduct(int[,] array2D)
+        {
+            int product = 0;
+            for (int i = 0; i < array2D.GetLength(0); i++)
+            {
+                for (int j = 0; j < array2D.GetLength(1); j++)
+                {
+                    if (i == array2D.GetLength(0) - 1)
+                    {
+                        while (j < array2D.GetLength(1) - 1)
+                        {
+                            if (array2D[i, j] * array2D[i, j + 1] > product)
+                            {
+                                product = array2D[i, j] * array2D[i, j + 1];
+                            }
+                        }
+                    }
+                    else if (j == 0)
+                    {
+                        if (true)
+                        {
+                            product = array2D[i, j] *
+                        }
+                    }
+                }
+            }
+            return product;
         }
     }
 }
