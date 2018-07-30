43eb2e8042d18e0425762f48465bc0966a683c1c
Calamario <mario.nishio@gmail.com>
2018-07-30 11:42:11 -0700

    readme, code, test

---
+++ Data_Structures/Sorting_Algorithms/InsertionSort/InsertionSort/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace InsertionSort
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = new int[5] { 100, 3, 15, 19, 40 };
+            foreach (int item in arr)
+            {
+                Console.WriteLine($"{item} =>");
+            }
+            int[] newArr = InsertionSorter(arr);
+            Console.WriteLine();
+            foreach (int item in newArr)
+            {
+                Console.WriteLine($"{item} =>");
+            }
+            Console.ReadLine();
+        }
+
+        public static int[] InsertionSorter(int[] arr)
+        {
+            int i = 1;
+            while (i < arr.Length)
+            {
+                int x = arr[i];
+                int j = i - 1;
+                while (j >= 0 && arr[j] > x)
+                {
+                    arr[j + 1] = arr[j];
+                    j--;
+                }
+                arr[j + 1] = x;
+                i++;
+            }
+            return arr;
+        }
+    }
+}
---
+++ Data_Structures/Sorting_Algorithms/InsertionSort/README.md
@@ -0,0 +1,12 @@
+# Insertion Sort
+Time: O(n^2)
+Space: O(1)
+
+Insertion Sort takes in an unsorted array and return 
+a sorted one. It starts with the index 1 and compare
+the values in index 0 and 1. If the value at 0 is 
+larger than the value at 1, it will swap the two, move 
+on to continue this pattern. a counter i and j is kept 
+track in a while loop.
+
+This can also be implemented with a recursion.
